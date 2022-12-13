b0319f9d30ab877429ee2d757aace2b43d6f1428
AngelinaPrisaznuk <114239162+AngelinaPrisaznuk@users.noreply.github.com>
2022-12-13 21:55:05 +0200

    Add project files.

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int* ptr = new int; // Динамічно виділяємо цілочисельну змінну.
+	int* otherPtr = ptr; // otherPtr тепер вказує на ту ж саму виділену пам'ятьб що і ptr.
+	delete ptr; // Повертаємо пам'ять назад в операційну системуб ptr i otherPtr тепер є висячим вказівником.
+	ptr = 0; // ptr тепер уже nullptr.
+	// Проте otherPtr як і раніше є висячим вказівником.
+	return 0;
+}
